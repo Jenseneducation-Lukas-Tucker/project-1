7af15c3d8614d9e8312583820bbd03ee4336ebc5
frotuck <54941820+frotuck@users.noreply.github.com>
2020-02-17 15:27:11 +0100

    todo-list
    using vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos" v-on:delete-todo="deleteTodo" v-on:edit-todo="editTodo"/>
+    <AddTodo v-on:add-todo="addTodo"/>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
+import AddTodo from './components/AddToDo.vue';
+
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Todos,
+    AddTodo,
+  },
+
+  data() {
+    return {
+      todos: []
+}
+  },
+
+ methods: {
+    addTodo(newTodoObj) {
+      this.todos = [...this.todos, newTodoObj];
+    },
+    deleteTodo(todoId) {
+      this.todos = this.todos.filter(todo => todo.id !== todoId);
+    },
+    editTodo(todoTitle){
+        this.todos = this.todos.edit(todo => todo.title == todoTitle);
+    }
+
+
   }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-bind:class="{ 'completed': todo.completed }">
+    <p v-on:click="markComplete">{{ todo.title }}</p>
+    <input type="text" v-model="todo.title">
+    <button @click="$emit('delete-todo', todo.id)">Delete</button>
+        <button @click="$emit('edit-todo', todo.title)">Edit</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Todo',
+  props: [
+    "todo"
+  ],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed
+    }
+
+  }
+}
+</script>
+<style scoped>
+  .completed {
+    text-decoration: line-through;
+  }
+</style>
